ea0d732aadf9b780ec57ebab15d2dc569050a7c6
Peter Young <young40@gmail.com>
2018-08-02 20:50:57 +0800

    05

--- 05.Transformations.playground/Contents.swift
+++ 05.Transformations.playground/Contents.swift
@@ -1,8 +1,13 @@
 import MetalKit
 import PlaygroundSupport
 
+struct Vertex {
+    var position: vector_float4
+    var color: vector_float4
+}
+
 class MyMetalView: MTKView {
-    private var vertexData: [Float]!
+    private var vertexData: [Vertex]!
     private var vertexBuffer: MTLBuffer!
     
     private var renderPipelineState: MTLRenderPipelineState!
@@ -20,12 +25,21 @@ class MyMetalView: MTKView {
     }
     
     public func initVertex() {
-        self.vertexData = [-0.7, -0.7, 0, 1,
-                           0.7, -0.7, 0, 1,
-                           0, 0.7, 0, 1]
+        self.vertexData = [Vertex(position: [   0,  0.7, 0, 1], color: [1, 0, 0, 1]),
+                           Vertex(position: [-0.7, -0.7, 0, 1], color: [0, 1, 0, 1]),
+                           Vertex(position: [ 0.7,  0.7, 0, 1], color: [0, 0, 1, 0])]
+        
+        let vertexDataSize = MemoryLayout<Vertex>.size * self.vertexData.count
+        
+        self.vertexBuffer = self.device?.makeBuffer(bytes: self.vertexData, length: vertexDataSize, options: [])
     }
     
-    
+    public func initShader() {
+        let shaderStr = """
+#include <metal_stdlib>
+
+"""
+    }
 }
 
 
