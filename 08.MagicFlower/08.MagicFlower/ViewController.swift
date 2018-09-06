72b772f4c81947051a0f85d34c5573af147f4b32
Peter Young <young40@gmail.com>
2018-09-06 22:23:00 +0800

    08

---
+++ 08.MagicFlower/08.MagicFlower/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  08.MagicFlower
+//
+//  Created by 杨世玲 on 2018/9/6.
+//  Copyright © 2018 杨世玲. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
