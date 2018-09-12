c131b32458830cfc9599a1b524fa5a392a46b058
Peter Young <young40@gmail.com>
2018-09-12 22:36:00 +0800

    update

--- 10.Noise/10.Noise/MyMetalView.swift
+++ 10.Noise/10.Noise/MyMetalView.swift
@@ -9,15 +9,56 @@
 import MetalKit
 
 class MyMetalView: MTKView {
+    var cps: MTLComputePipelineState!
+    
     var cmdQueue: MTLCommandQueue!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
         
+        initShader()
+    }
+    
+    func initShader() {
+        device = MTLCreateSystemDefaultDevice()!
         
+        framebufferOnly = false
+        
+        cmdQueue = device?.makeCommandQueue()
+        
+        let lib = device?.makeDefaultLibrary()
+        
+        let computer = lib?.makeFunction(name: "computer")!
+        
+        do {
+            cps = try device?.makeComputePipelineState(function: computer!)
+        } catch let e {
+           fatalError("\(e)")
+        }
     }
     
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
+        
+        let drawable = currentDrawable!
+        
+        let cmdBuffer = cmdQueue.makeCommandBuffer()!
+        
+        let cmdEncoder = cmdBuffer.makeComputeCommandEncoder()!
+        
+        cmdEncoder.setTexture(drawable.texture, index: 0)
+        cmdEncoder.setComputePipelineState(cps)
+        
+        let threadPreGid = MTLSizeMake(8, 8, 1)
+        let threadCount  = MTLSizeMake((drawable.texture.width + threadPreGid.width - 1) / threadPreGid.width,
+                                       (drawable.texture.height + threadPreGid.height - 1) / threadPreGid.height
+                                       , 1)
+        
+        cmdEncoder.dispatchThreadgroups(threadCount, threadsPerThreadgroup: threadPreGid)
+        
+        cmdEncoder.endEncoding()
+        
+        cmdBuffer.present(drawable)
+        cmdBuffer.commit()
     }
 }
--- 10.Noise/10.Noise/MyShader.metal
+++ 10.Noise/10.Noise/MyShader.metal
@@ -9,4 +9,7 @@
 #include <metal_stdlib>
 using namespace metal;
 
-
+kernel void computer(texture2d<float, access::write> output [[texture(0)]],
+                     uint2 gid [[thread_position_in_grid]]) {
+    output.write(float4(0.3, 0.4, 0.5, 1), gid);
+}
