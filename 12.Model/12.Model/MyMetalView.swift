9edddbe97e15f11efec12de2cbc4d51c0e68d4d3
Peter Young <young40@gmail.com>
2018-09-22 10:01:17 +0800

    update

--- 12.Model/12.Model/MyMetalView.swift
+++ 12.Model/12.Model/MyMetalView.swift
@@ -9,5 +9,36 @@
 import MetalKit
 
 class MyMetalView: MTKView {
+    var cmdQueue: MTLCommandQueue!
+    var renderPipelineState: MTLRenderPipelineState!
     
+    var uniformsBuffer: MTLBuffer!
+    var meshes: [MTKMesh]!
+    var texture: MTLTexture!
+    
+    var depthStencilState: MTLDepthStencilState!
+    let vertexDescriptor = MTLVertexDescriptor()
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.clearColor = MTLClearColorMake(0.5, 0.5, 0.5, 1)
+        self.colorPixelFormat = .bgra8Unorm
+        
+        initMetalObjs()
+        
+    }
+    
+    func initMetalObjs() {
+        device = MTLCreateSystemDefaultDevice()!
+        cmdQueue = device!.makeCommandQueue()
+        
+        self.depthStencilPixelFormat = MTLPixelFormat.depth32Float_stencil8
+        
+        let stencilDescriptor = MTLDepthStencilDescriptor()
+        stencilDescriptor.depthCompareFunction = MTLCompareFunction.less
+        stencilDescriptor.isDepthWriteEnabled = true
+        
+        depthStencilState = device?.makeDepthStencilState(descriptor: stencilDescriptor)
+    }
 }
